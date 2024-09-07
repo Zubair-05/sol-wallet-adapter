72b3b9cffd1342f945d4dea8d97c4d819ebb1c32
Zubair-05 <mullamdz0501@gmail.com>
2024-09-08 00:27:55 +0530

    added an alert for the error

--- dapp-starter/src/components/Airdrop.jsx
+++ dapp-starter/src/components/Airdrop.jsx
@@ -10,6 +10,7 @@ const Airdrop = () => {
             console.log(response);
         } catch (e) {
             console.log(e)
+            alert("and now we got an error message")
         }
     }
     return (
